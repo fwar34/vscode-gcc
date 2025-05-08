7f1ea81a2d4c87db2ae63ce9b6d7d666e410b8f5
fwar34 <fwar34@126.com>
2025-05-09 00:12:57 +0800

    first push

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string test;
+    cout << test.size() << endl;
+    vector<int> vc = {1, 3, 1, 3};
+    map<int, int> mm;
+    cout << mm.count(1) << endl;
+
+    return 0;
+}
